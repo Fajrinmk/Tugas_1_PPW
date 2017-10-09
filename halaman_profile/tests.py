b69a066bdab7d7c6d49ab378d4facac31047ad74
Safira Amalia <safira.amalia@gmail.com>
2017-10-09 10:06:57 +0700

    tes profil 1

--- halaman_profile/tests.py
+++ halaman_profile/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from django.test import Client
+from django.urls import resolve
+from django.http import HttpRequest
+from halaman_profile.views import index
 # Create your tests here.
+
+class halaman_profile(TestCase){
+    def test_halaman_profile_is_exist(self):
+        response = Client().get('/halaman_profile/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_lab4_using_index_func(self):
+        found = resolve('/halaman_profile/')
+        self.assertEqual(found.func, index)
+
+
+
+
+
+}
--- halaman_profile/views.py
+++ halaman_profile/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponseRedirect
 
 response = {'author': "Hepzibah Smith"}
 birthday = '01 Jan'
 gender = 'Female'
-expertise = ['Marketing','Collector','Public Speaking']
+expertise = 'Marketing Collector Public Speaking'
 description = 'Antique expert. Experience as marketer for 10 years'
 email = 'hello@smith.com'
 
@@ -13,7 +12,7 @@ def index(request):
     html = 'halaman_profile/halaman_profile.html'
     response['birthday'] = birthday
     response['gender'] = gender
-    # response['expertise'] = expertise  
+    response['expertise'] = expertise  
     response['description'] = description
     response['email'] = email  
     return render(request, 'halaman_profile.html', response)
