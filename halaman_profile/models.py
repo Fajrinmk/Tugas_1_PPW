73116ca1627460e6dbcb1f6257e792e2f590028f
fajrinmk <fajrinmk@gmail.com>
2017-10-10 21:09:43 +0700

    benerin base.html

--- halaman_profile/models.py
+++ halaman_profile/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Expertise (models.Model):
-     # dataprofile = models.ForeignKey(DataProfil)
+     #dataprofile = models.ForeignKey(DataProfil)
      expertise = models.CharField(max_length=100)
 
      def __str__(self):
