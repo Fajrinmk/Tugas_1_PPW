c6e7b7a82499015f80bb39e73278e11ebf8deaa9
JosuaChristanto <jossi_christ@ymail.com>
2017-10-11 19:59:01 +0700

    add_friend hampir kelar

--- add_friend/models.py
+++ add_friend/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.utils import timezone
+import pytz
 
 # Create your models here.
 class new_friend(models.Model):
+	def converTime():
+		return timezone.now() + timezone.timedelta(hours=7)
 	name = models.CharField(max_length=27)
 	heroku_link = models.CharField(max_length=27)
-	created_date = models.DateTimeField(auto_now_add=True)
+	created_date = models.DateTimeField(default=converTime)
+
--- add_friend/tests.py
+++ add_friend/tests.py
@@ -16,3 +16,21 @@ def test_add_friend_url_using_html(self):
 def test_add_friend_url_using_index(self):
 	found = resolve('/add_friend/')
 	self.assertEqual(found.func,index)
+
+def test_delete_feature(self):
+        #digunakan untuk mengecek apakah linknya dari add_todo aktif
+    test = "ini adalah test saja"
+    response_post = Client().post('/add_friend/tambah_teman', {'name': test, 'heroku_link': 'www.google.com'}) #post(link metodrnya, isi bi=uat si metod)
+    self.assertEqual(response_post.status_code,302)
+
+    response = Client().get('/add_friend/')
+    html_response = response.content.decode('utf8')
+    self.assertIn(test, html_response)     
+        
+
+    response = Client().post('/add_friend/delete/1/')
+    self.assertEqual(response.status_code,302)
+
+    response = Client().get('/add_friend/')
+    html_response = response.content.decode('utf8')
+    self.assertNotIn(test, html_response)
