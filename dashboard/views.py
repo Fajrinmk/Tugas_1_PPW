809aa1f7e6124bc52a669529a7b232e92fa4b4af
fajrinmk <fajrinmk@gmail.com>
2017-10-12 02:27:11 +0700

    duh error

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,9 +7,9 @@ def index(request):
     response = {}
     response['profile_name'] = profile_name
     stat = Status.objects.all().order_by('-id')
-    # if len(stat) == 0:
-    # 	response['database'] = "Belum ada status"
-    # else:
-    response['database'] = stat[0]    
+    if len(stat) == 0:
+    	response['database'] = "Belum ada status"
+    else:
+    	response['database'] = stat[0]    
     response['feed'] = len(stat)
     return render(request, 'dashboard/dashboard.html', response)
