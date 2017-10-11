6b87c29ab99b206dc01cdfd6bdbe4793d093eba0
JosuaChristanto <jossi_christ@ymail.com>
2017-10-11 19:26:07 +0700

    add_friend hampir kelar

--- add_friend/models.py
+++ add_friend/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class new_friend(models.Model):
 	name = models.CharField(max_length=27)
 	heroku_link = models.CharField(max_length=27)
+	created_date = models.DateTimeField(auto_now_add=True)
