5e78cfbbd9b8c9a09d6d3a81d16630a794cf420b
fajrinmk <fajrinmk@gmail.com>
2017-10-12 02:31:41 +0700

    tambah tdd dashboard dulu deh

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
+from .views import index
 
-# Create your tests here.
+class DashboardUnitTest(TestCase):
+    def test_dashboard_url_is_exist(self):
+        response = Client().get('/dashboard/')
+        self.assertEqual(response.status_code,200)
+
+    def test_using_index_func(self):
+        found = resolve('/dashboard/')
+        self.assertEqual(found.func, index)
