5ece262bc63982002cce322f246609f905865211
JosuaChristanto <jossi_christ@ymail.com>
2017-10-11 14:34:06 +0700

    add_friend sudah hampir jadi

--- add_friend/urls.py
+++ add_friend/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import index, tambah_teman
+from .views import index, tambah_teman,delete
 
 
 urlpatterns=[
 	url(r'^$', index, name='index'),
 	url(r'^tambah_teman', tambah_teman, name='tambah_teman'),
+	url(r'^delete/(?P<id>\d+)/$',delete, name='delete'),
 ]
