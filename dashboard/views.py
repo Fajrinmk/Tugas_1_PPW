f8b213e52eaaa0b454e58c505e201893ae90d4fe
fajrinmk <fajrinmk@gmail.com>
2017-10-11 15:43:34 +0700

    tambah stats

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from update_status.models import Status
+from halaman_profile.views import profile_name
 
 def index(request):
-    response = {'name': "Fajrin", 'age': "17"}
+    response = {}
+    response['profile_name'] = profile_name
+    stat = Status.objects.all().order_by('-id')
+    response['database'] = stat[0]
+    response['feed'] = len(stat)
     return render(request, 'dashboard/dashboard.html', response)
