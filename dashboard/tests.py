05f0234857cee58d736ee7cc059318b814e8f608
fajrinmk <fajrinmk@gmail.com>
2017-10-13 15:29:48 +0700

    coverage dashboard 100% yey

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from django.urls import resolve
+from update_status.models import Status
 from .views import index
 
 class DashboardUnitTest(TestCase):
@@ -11,3 +12,16 @@ class DashboardUnitTest(TestCase):
     def test_using_index_func(self):
         found = resolve('/dashboard/')
         self.assertEqual(found.func, index)
+
+    def test_if_user_havent_posted(self):
+        message = "You have not posted yet"
+        response = Client().get('/dashboard/')
+        html_response = response.content.decode('utf8')
+        self.assertIn(message, html_response)
+
+    def test_if_user_have_posted(self):
+        message = "You have not posted yet"
+        tryStatus = Status.objects.create(status='woyyy hehehe')
+        response = Client().get('/dashboard/')
+        html_response = response.content.decode('utf8')
+        self.assertNotIn(message,html_response)
